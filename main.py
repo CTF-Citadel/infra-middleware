08d8c3e902b2bea3b80660decbae9f01e220d7d9
felixslama <firejava@protonmail.com>
2023-09-29 13:08:23 +0200

    added post endpoint for container creation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import composer
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello Hacker"}
+
+@app.post("/container")
+async def spawn_container(compose_file: str):
+    """
+        This function can be used to spawn new containers according to a specified compose file
+    """
+    return composer.spawn_container(compose_file)
+
