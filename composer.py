e3dc384bf08d534355d2a0203da6ffa7e5e7c358
felixslama <firejava@protonmail.com>
2023-09-29 13:07:57 +0200

    changed file path prefix for docker-compose

--- composer.py
+++ composer.py
@@ -1,3 +1,4 @@
 import subprocess
-def spawn_container(path_to_compose_file):
+def spawn_container(challenge):
+    path_to_compose_file = "challenges/" + challenge
     subprocess.run(["docker-compose", "-f", path_to_compose_file, "up","-d"])
