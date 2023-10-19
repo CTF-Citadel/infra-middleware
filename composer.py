bb718a589ad10279a9ae294d45494a27a71ffb0d
felixslama <firejava@protonmail.com>
2023-10-19 21:56:45 +0200

    add get container info

--- composer.py
+++ composer.py
@@ -1,4 +1,10 @@
 import subprocess
-def spawn_container(challenge):
+def spawn_challenge(challenge, environment_variables=None):
+    """
+    Spawn a new instance of a challenge.
+    :param challenge: provide the name of the docker-compose file for the challenge"
+    :param environment_variables: provide the environment variables for the docker-compose file as dictionary.
+    :return: output of detached compose command
+    """
     path_to_compose_file = "challenges/" + challenge
-    subprocess.run(["docker-compose", "-f", path_to_compose_file, "up","-d"])
+    return subprocess.run(["docker-compose", "-f", path_to_compose_file, "up","-d"], env=environment_variables)
--- main.py
+++ main.py
@@ -9,9 +9,14 @@ async def root():
     return {"message": "Hello Hacker"}
 
 @app.post("/container")
-async def spawn_container(compose_file: str):
+async def spawn_challenge(compose_file: str, environment_variables: dict):
     """
         This function can be used to spawn new containers according to a specified compose file
     """
-    return composer.spawn_container(compose_file)
+    return composer.spawn_challenge(compose_file, environment_variables)
 
+@app.get("/container")
+async def container_details(container_id: str):
+    """
+        This endpoint returns infos about a specified container. Such as environment variables
+    """
