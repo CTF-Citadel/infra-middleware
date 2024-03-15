789bdd6bb310bf281aa4e7abddd403ed92b4ffa5
felixslama <firejava@protonmail.com>
2024-03-15 10:29:27 +0100

    enable env variables again

--- composer.py
+++ composer.py
@@ -23,12 +23,12 @@ def spawn_challenge(challenge, environment_variables=None):
         #create instance network
         #subprocess.run(["docker", "network", "create", instance_id])
         #generate a random unique network port for the web app
-        #environment_variables = json.loads(environment_variables)
+        environment_variables = json.loads(environment_variables)
         print("oida")
         #build a response that returns instance id, challenge and all env vars
-        subprocess.run(["docker-compose", "-f", "instances/" + instance_id + "/docker-compose.yml", "build"])
-        subprocess.run(["docker-compose", "-f", "instances/" + instance_id + "/docker-compose.yml", "push"])
-        subprocess.run(["docker", "stack","deploy", "--compose-file", "instances/" + instance_id + "/docker-compose.yml", instance_id])
+        subprocess.run(["docker-compose", "-f", "instances/" + instance_id + "/docker-compose.yml", "build"],env=environment_variables)
+        subprocess.run(["docker-compose", "-f", "instances/" + instance_id + "/docker-compose.yml", "push"],env=environment_variables)
+        subprocess.run(["docker", "stack","deploy", "--compose-file", "instances/" + instance_id + "/docker-compose.yml", instance_id],env=environment_variables)
 
         environment_variables["IP"] = str(socket.gethostbyname(socket.gethostname()))
         response = {
